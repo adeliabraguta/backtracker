6dace2bd74263d40561222278e6f1fe0fbf51717
adeliabraguta <adeliabraguta@gmail.com>
2025-01-28 15:20:06 +0200

    Fix routing for project table

--- src/main/webapp/app/entities/entity.routes.ts
+++ src/main/webapp/app/entities/entity.routes.ts
@@ -7,20 +7,9 @@ const routes: Routes = [
     loadChildren: () => import('./admin/authority/authority.routes'),
   },
   {
-    path: 'projects',
-    loadComponent: () => import('./projects-management/table/projects-management.component'),
-  },
-  {
-    path: 'update/:id',
-    loadComponent: () => import('./projects-management/update/project-management-update.component'),
-  },
-  {
-    path: 'new',
-    loadComponent: () => import('./projects-management/update/project-management-update.component'),
-  },
-  {
-    path: 'detail/:id',
-    loadComponent: () => import('./projects-management/detail/project-management-detail.component'),
+    path: 'project-management',
+    loadChildren: () => import('./projects-management/projects-management.route'),
+    title: 'projectManagement.home.title',
   },
 ];
 
